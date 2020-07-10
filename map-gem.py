12b8818043f125eafecc4460cced47843373bc1a
jeads <60721703+jeads92@users.noreply.github.com>
2020-07-10 18:13:40 -0400

    initial tests

---
+++ map-gem.py
@@ -0,0 +1,23 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+
+kivy.require('1.11.1')
+
+
+class LoginScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)  # Init method runs gridlayout init.
+        self.add_widget(Label(text='New User'))
+
+
+class MapGem(App):
+    def build(self):
+        return LoginScreen()
+
+
+if __name__ == '__main__':
+    MapGem().run()
