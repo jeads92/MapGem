2cc2af5fa1987b90c6cdaf5dca8dc5c8579b0c64
jeads <60721703+jeads92@users.noreply.github.com>
2020-07-19 17:11:27 -0400

    Update on July 19 2020

--- map-gem.py
+++ map-gem.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 
 kivy.require('1.11.1')
@@ -11,9 +12,21 @@ kivy.require('1.11.1')
 class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)  # Init method runs gridlayout init.
-        self.add_widget(Label(text='New User'))
-        self.add_widget(Label(text='Login'))
-        self.add_widget(Label(text='Skip Login'))
+        self.cols = 1
+
+        self.new_user_btn = Button(text='New User')
+        self.new_user_btn.bind(on_press=self.test)
+        self.add_widget(self.new_user_btn)
+
+        self.login_btn = Button(text='Login')
+        self.add_widget(self.login_btn)
+
+        self.skip_btn = Button(text='Skip Login')
+        self.add_widget(self.skip_btn)
+
+    def test(self, instance):
+        # Temporary function to test if bind works
+        print('Test has been run')
 
 
 class MapGem(App):
