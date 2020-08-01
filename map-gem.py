14934cdd9068c1192f5622fd258ffba7fc8fe482
jeads <60721703+jeads92@users.noreply.github.com>
2020-08-01 09:26:45 -0400

    Added a kv file to separate logic and style.

--- map-gem.py
+++ map-gem.py
@@ -1,9 +1,6 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import Screen
 
@@ -12,82 +9,28 @@ kivy.require('1.11.1')
 
 
 class LoginScreen(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)  # Init method runs gridlayout init.
-        self.cols = 1
 
-        self.new_user_btn = Button(text='New User')
-        self.new_user_btn.bind(on_press=self.to_newuser)
-        self.add_widget(self.new_user_btn)
-
-        self.login_btn = Button(text='Login')
-        self.login_btn.bind(on_press=self.to_userlogin)
-        self.add_widget(self.login_btn)
-
-        self.skip_btn = Button(text='Skip Login')
-        self.skip_btn.bind(on_press=self.to_mainmenu)
-        self.add_widget(self.skip_btn)
-
-    def to_newuser(self, instance):
+    def to_newuser(self):
         main_app.screen_manager.current = 'New Account'
         # yes
 
-    def to_userlogin(self, instance):
+    def to_userlogin(self):
         main_app.screen_manager.current = 'User Login'
 
-    def to_mainmenu(self, instance):
+    def to_mainmenu(self):
         main_app.screen_manager.current = 'Main Screen'
 
 
 class UserLogin(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 2
-
-        self.add_widget(Label(text='Login to Account'))
-        self.add_widget(Label(text=""))
-
-        self.add_widget(Label(text='Username'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-
-        self.add_widget(Label(text='Password'))
-        self.password = TextInput(multiline=False,
-                                  password=True)
-        self.add_widget(self.password)
+    pass
 
 
 class NewAccount(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 2
-
-        self.add_widget(Label(text='Create New Account'))
-        self.add_widget(Label(text=""))
-
-        self.add_widget(Label(text="Username"))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-
-        self.add_widget(Label(text="Password"))
-        self.password = TextInput(multiline=False,
-                                  password=True)
-        self.add_widget(self.password)
+    pass
 
 
 class MainScreen(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 1
-        # Need to display the username in the topright corner
-        self.new_map = Button(text='Create New Maps')
-        self.add_widget(self.new_map)
-
-        self.view_maps = Button(text='View Maps')
-        self.add_widget(self.view_maps)
-
-        self.options = Button(text='Options')
-        self.add_widget(self.options)
+    pass
 
 
 class MapGem(App):
