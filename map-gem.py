103d448f078f3137f888eca1f80250cb17754e6e
jeads <60721703+jeads92@users.noreply.github.com>
2020-08-03 17:26:44 -0400

    Added more screens for basic layout of the Application.

--- map-gem.py
+++ map-gem.py
@@ -8,6 +8,9 @@ from kivy.properties import ObjectProperty
 
 kivy.require('1.11.1')
 
+def screen_changer(instance):
+    ''' Switches to the screen based off of the screen name.'''
+    pass
 
 class EntryScreen(GridLayout):
 
@@ -26,14 +29,14 @@ class UserLogin(GridLayout):
     name1 = ObjectProperty(None)
     password1 = ObjectProperty(None)
 
-    def to_mainmenu(self):
-        # the print function is a test. deleat it after
-        # concept for ids and vars is understood.
-        print(f'Name: {self.name1.text}, password: {self.password1.text}')
+    def to_entryscreen(self):
         main_app.screen_manager.current = 'Entry Screen'
 
     def to_login(self):
         main_app.screen_manager.current = 'Login Screen'
+
+    def to_mainscreen(self):
+        main_app.screen_manager.current = 'Main Screen'
 
 
 class NewAccount(GridLayout):
@@ -42,10 +45,29 @@ class NewAccount(GridLayout):
 
 
 class MainScreen(GridLayout):
+    def to_options(self):
+        main_app.screen_manager.current = 'Options'
+        
+    def to_editmap(self):
+        main_app.screen_manager.current = 'Edit Map'
+        
+    def to_viewmaps(self):
+        main_app.screen_manager.current = 'View Maps'
+        
+class EditMap(GridLayout):
+    pass
+
+
+
+class Options(GridLayout):
+    pass
+
+class ViewMaps(GridLayout):
     pass
 
 
 class MapGem(App):
+    ''' Builds all of the screens as instances. '''
     def build(self):
         self.screen_manager = ScreenManager()
 
@@ -67,6 +89,21 @@ class MapGem(App):
         self.mainscreen = MainScreen()
         screen = Screen(name='Main Screen')
         screen.add_widget(self.mainscreen)
+        self.screen_manager.add_widget(screen)
+        
+        self.options = Options()
+        screen = Screen(name='Options')
+        screen.add_widget(self.options)
+        self.screen_manager.add_widget(screen)
+        
+        self.edit_map = EditMap()
+        screen = Screen(name='Edit Map')
+        screen.add_widget(self.edit_map)
+        self.screen_manager.add_widget(screen)
+        
+        self.view_maps = ViewMaps()
+        screen = Screen(name='View Maps')
+        screen.add_widget(self.view_maps)
         self.screen_manager.add_widget(screen)
 
         return self.screen_manager
